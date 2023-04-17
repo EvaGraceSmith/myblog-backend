ce0f4e029842175dc9d770eb3cbd0a519aab38c5
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-17 11:42:33 -0500

    added comments to global

--- global.js
+++ global.js
@@ -13,5 +13,8 @@ const int = setInterval(() => {
 
 }, 1000);
 
+//this is useful to get the directory name and file name so you can use it in other files
 console.log(__dirname);
 console.log(__filename);
+
+//node does not have a window object, but it does have a global object, so you can't access document or querySelector.  You can access the global object, but it is not recommended.  You can use the global object to access the setTimeout and setInterval functions.
