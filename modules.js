10eb2567a44015c3234db885da1dc6e19b905157
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-17 13:20:43 -0500

    added os

--- modules.js
+++ modules.js
@@ -1,4 +1,9 @@
-const abc = require('./people');
+//rather than giving the const a name, we can use destructuring to pull out the properties we want by adding an object {} to the require function.  This is useful when you have a lot of properties ('people' in this instance) in the object and you only want to use a few of them.
 
-console.log(abc);
+const {people, ages} = require('./people');
+
+console.log(people, ages);
+
+//os is a built in module that gives you information about the operating system you are using.  You can use this to get the hostname, platform, and other information about the operating system.  You can also use this to get the free memory, total memory, and other information about the memory on your computer.  You can also use this to get the current user, and other information about the user.
+const os = require('os');
 
