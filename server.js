b3a2b194db567a81b96a5b25739b9586f2a1e194
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-18 09:11:56 -0500

    added about page

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>hello, world!</p>');
+    res.end();
 });
 
 server.listen(3010
