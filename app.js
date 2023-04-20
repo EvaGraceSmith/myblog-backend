c24e202c9012b59e62b1d27523fea290df0d8ef2
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-20 10:15:45 -0500

    updated app.get

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ app.listen(3000);
 
 //installed ejs, a templating engine that allows you to use javascript in html files and allows you to use variables (dynamic content) in html files
 
+//register view engine
+app.set('view engine', 'ejs');
+
 
 //this function takes in a request and a response object.  The request object contains information about the request that was made.  The response object contains information about what we want to send back to the client.
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+      res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
