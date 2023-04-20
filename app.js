d423e5f85a2c3c551675d9a0da20cf9bcbc785ff
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-20 08:53:10 -0500

    added routing

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const app = express();
 
 //listen for requests
 app.listen(3000);
+
+
+//this function takes in a request and a response object.  The request object contains information about the request that was made.  The response object contains information about what we want to send back to the client.
+app.get('/', (req, res) => {
+    res.send('<p>home page</p>');
+    });
+
+    app.get('/about', (req, res) => {
+        res.send('<p>about page</p>');
+        });
