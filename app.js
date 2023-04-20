98dd8551da00d4dd30ef5d34f606e528fd4f45b5
EvaGraceSmith <smithevagrace@gmail.com>
2023-04-20 09:41:49 -0500

    updated 404

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ app.listen(3000);
 
 //this function takes in a request and a response object.  The request object contains information about the request that was made.  The response object contains information about what we want to send back to the client.
 app.get('/', (req, res) => {
-   res.sendFile('./views/index.html', { root: __dirname });
-    });
+    res.sendFile('./views/index.html', { root: __dirname });
+});
 
-    app.get('/about', (req, res) => {
-        res.sendFile('./views/about.html', { root: __dirname });
-        });
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.reroute('/about');
+});
+
+//404 page
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname });
+});
